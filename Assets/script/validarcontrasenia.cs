4ba7875623f0dbac58532b14cac147eb784c8817
aritrajtman <47066506@est.ort.edu.ar>
2022-04-22 03:45:57 -0300

    Initial commit

---
+++ Assets/script/validarcontrasenia.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class validarcontrasenia : MonoBehaviour
+{
+    string contraseñacorrecta;
+    string contraseñausuario;
+     public Text ingresousuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contraseñacorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarcontrasenias()
+    {
+        contraseñausuario = ingresousuario.text;
+        if(contraseñausuario == contraseñacorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "bienvenido";
+            Debug.Log("bienvenido");
+        }
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "contraseña incorrecta";
+            Debug.Log("contraseña incorrecta");
+        }
+    }
+}
